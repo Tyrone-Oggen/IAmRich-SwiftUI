4ea449ca4ed05e4e37ccc574ab0418e706a6b955
Tyrone Oggen <oggen.tyronekyle@gmail.com>
2021-09-07 22:04:20 +0200

    project init

---
+++ IAmRich/IAmRichApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichApp.swift
+//  IAmRich
+//
+//  Created by Tyrone Oggen on 2021/09/07.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
